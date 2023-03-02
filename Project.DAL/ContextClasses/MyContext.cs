46077bf15152dee607ece04ea5a8c2e77aa7013b
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-02 11:27:18 +0300

    DAL katmanında MyContext class'i ContextClasses klasoru icerisinde acıldı

---
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClasses
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}

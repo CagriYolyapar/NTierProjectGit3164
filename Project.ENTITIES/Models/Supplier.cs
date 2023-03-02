21afb4853a2a89725d0ca08591b7db4576d16ec3
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-02 11:10:54 +0300

    Entities klasorunde Supplier class'ı olusturuldu

---
+++ Project.ENTITIES/Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Supplier:BaseEntity
+    {
+        public string CompanyName { get; set; }
+
+        //Relational Properties
+    }
+}
